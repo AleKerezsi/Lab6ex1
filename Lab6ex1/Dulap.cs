600c2e751f322cc58cafcf7d43d8de34e7dcea00
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2023-01-30 19:41:49 +0200

    Add project files.

---
+++ Lab6ex1/Dulap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab6ex1
+{
+   public class Dulap
+    {
+        private int lungime;
+        private int latime;
+        private int inaltime;
+
+        public Dulap(int lungime, int latime, int inaltime)
+        {
+            this.lungime = lungime;
+            this.latime = latime;
+            this.inaltime = inaltime;
+        }
+
+        public int CalculeazaVolum()
+        {
+            int volumDulap = lungime * latime * inaltime;
+            return volumDulap;
+        }
+    }
+}
